d7f2855f685d8e0352a0d74e4b7be8176d6266ee
Matthias Pfeil <matthias.pfeil@wwu.de>
2015-09-24 17:35:44 +0200

    basic project structure

---
+++ Gruntfile.js
@@ -0,0 +1,104 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  require('time-grunt')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    haml: {
+      dist: {
+        files: {
+          'dist/index_test.html': 'app/index.haml'
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          require('autoprefixer')({
+              browsers: ['last 2 versions']
+          })
+        ]
+      },
+      dist: {
+        src: 'dist/assets/css/batman.css'
+      }
+    },
+
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'app/assets/scss',
+          cssDir: 'dist/assets/css',
+          environment: 'development',
+          outputStyle: 'expanded'
+        }
+      }
+    },
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      haml: {
+        files: ['app/index.haml'],
+        tasks: ['haml:dist'],
+        options: {
+          livereload: true
+        }
+      },
+      scss: {
+        files: ['app/assets/scss/batman.scss'],
+        tasks: ['compass','postcss:dist'],
+        options: {
+          livereload: true
+        }
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          'dist/index.html',
+        ]
+      }
+    },
+
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 1337,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: {
+            path: 'dist',
+            options: {
+              index: 'index_test.html'
+            }
+          }
+        }
+      }
+    },
+  });
+
+  grunt.registerTask('serve', function (target) {
+    grunt.task.run([
+      'haml:dist',
+      'compass',
+      'postcss:dist',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+};
